0bae5de3fe6ce216cdd1196d5eb1bd7be8aaa886
Laymon, Michael <michaellaymon@my.unt.edu>
2023-02-14 10:21:39 -0600

    WIP: now compiles successfully now uses server-side only `fetch` instead of 'fs' causes a TypeError when reading 'undefined' from "data"

--- api/aes.js
+++ api/aes.js
@@ -1,5 +1,16 @@
+//'use server';
+
+
 const crypto = require('crypto');
-const fs = require("fs").promises;
+// const fs = require("fs");
+
+
+// function that only runs on the server (where 'fs' is available) to fetch the json file
+export async function getServerSideProps() {
+    const response = await fetch(`../security/aesConfig.json`)
+    const data = await response.json()
+    return { props: { data } }
+}
 
 
 // generate key and initialization vector
@@ -11,36 +22,16 @@ const generateKeyAndIV = (plaintext, salt, iterations, keyLength, initialization
 };
 
 
-const encrypt = (plaintext) => {
+const encrypt = (plaintext, { data }) => {
     // const aesConfig = JSON.parse(fs.readFileSync("../security/aesConfig.json", 'utf8')); // get configuration for the encryption algorithm
-    const aesConfig = readFile("../security/aesConfig.json");
+    const aesConfig = data; // get configuration for the encryption algorithm by using getServerSideProps to read the file
 
     let plainbytes = new TextEncoder().encode(plaintext); // encode the plaintext string as bytes
     let { key, iv } = generateKeyAndIV(plainbytes, aesConfig.salt, aesConfig.iterations, aesConfig.keyLength, aesConfig.initializationVectorLength); // generate key and initialization vector
-    let cipher = crypto.createCipheriv("aes-256-cbc", key, iv); // create a cipher object
+    let cipher = crypto.createCipheriv('aes-256-cbc', key, iv); // create a cipher object
     let cipherbytes = cipher.update(plainbytes); // encrypt the plaintext bytes
     return Buffer.concat([cipherbytes, cipher.final()]); // return the encrypted bytes and invalidates the cipher object
 };
-
-
-export async function getStaticProps(path) {
-    const data = fs.readFileSync(path, "utf8");
-
-    return {
-        props: { data }
-    };
-}
-
-
-async function readFile(filePath) {
-    try {
-        const data = await fs.readFile(filePath);
-        return data.JSON.parse();
-    }
-    catch (error) {
-        console.error("An error occured when reading the file " + error.message);
-    }
-}
 
 
 export default encrypt;
