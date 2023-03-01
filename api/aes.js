a05d15debc71d2de7f81d9a62c465e4d32942e45
Laymon, Michael <michaellaymon@my.unt.edu>
2023-03-01 16:44:17 -0600

    Use env variables instead of reading local file Fixes for implementation mentioned above Add decrypt function for use later Add debug timers to encryption/decryption funcs Mark both functions to export; can no longer use default TODO: Encode URL in hex to be URL compatible

--- api/aes.js
+++ api/aes.js
@@ -1,38 +1,53 @@
-//'use server';
 
 
-const crypto = require('crypto');
-// const fs = require("fs");
-const aesConfig = getServerSideProps(); // get configuration for the encryption algorithm by using getServerSideProps to read the file
+const crypto = require('crypto'); // needed for the AES encryption/decryption
 
 
-// function that only runs on the server (where 'fs' is available) to fetch the json file
+// get the AES configuration from the .env file(s)
 export async function getServerSideProps() {
-    const response = await fetch(`../security/aesConfig.json`, { method: 'GET' , headers: { 'Content-Type': 'application/json' } });
-    const data = await response.json();
-    return data;
+    return {
+        salt: process.env.AES_SALT,
+        iterations: process.env.AES_ITERATIONS,
+        keyLength: process.env.AES_LEN_KEY,
+        initializationVectorLength: process.env.AES_LEN_INIT_VEC,
+    };
 }
 
 
-// generate key and initialization vector
+// generate key and initialization vector for the AES encryption
 const generateKeyAndIV = (plaintext, salt, iterations, keyLength, initializationVectorLength) => {
-    const byteSize = 8;
-    const key = crypto.pbkdf2Sync(plaintext, salt, iterations, keyLength / byteSize, String("sha"+keyLength));
-    const iv = crypto.randomBytes(initializationVectorLength / byteSize);
+    const BYTE_SIZE = 8;
+    const key = crypto.pbkdf2Sync(plaintext, salt, iterations, keyLength / BYTE_SIZE, String("sha"+keyLength));
+    const iv = crypto.randomBytes(initializationVectorLength / BYTE_SIZE);
     return { key, iv };
 };
 
 
-async function encrypt (plaintext) {
-    // const aesConfig = JSON.parse(fs.readFileSync("../security/aesConfig.json", 'utf8')); // get configuration for the encryption algorithm
-    // const aesConfig = data; // get configuration for the encryption algorithm by using getServerSideProps to read the file
-    
+// encrypt the plaintext string using AES
+export async function encrypt (plaintext) {
+    console.time('encrypt');
     let plainbytes = new TextEncoder().encode(plaintext); // encode the plaintext string as bytes
-    let { key, iv } = generateKeyAndIV(plainbytes, aesConfig.salt, aesConfig.iterations, aesConfig.keyLength, aesConfig.initializationVectorLength); // generate key and initialization vector
+    const aesConfig = await getServerSideProps();
+    let { key, iv } = generateKeyAndIV(plainbytes, aesConfig.salt, aesConfig.iterations, aesConfig.keyLength, aesConfig.initializationVectorLength); // generate key and initialization vector // the constants are defined in the .env file(s)
     let cipher = crypto.createCipheriv('aes-256-cbc', key, iv); // create a cipher object
     let cipherbytes = cipher.update(plainbytes); // encrypt the plaintext bytes
-    return Buffer.concat([cipherbytes, cipher.final()]); // return the encrypted bytes and invalidates the cipher object
+    cipherbytes = Buffer.concat([cipherbytes, cipher.final()]); // apply the encryption and destroy the cipher object
+    let ciphertext = new TextDecoder().decode(cipherbytes); // decode the encrypted bytes as a string
+    console.timeEnd('encrypt');
+    return String(ciphertext); // return the encrypted bytes and invalidates the cipher object
 };
 
 
-export default encrypt;
+// decrypt the ciphertext string using AES
+export async function decrypt (ciphertext) {
+    console.time('decrypt');
+    let cipherbytes = Buffer.from(ciphertext); // convert the ciphertext string to bytes
+    const aesConfig = await getServerSideProps();
+    let { key, iv } = generateKeyAndIV(cipherbytes, aesConfig.salt, aesConfig.iterations, aesConfig.keyLength, aesConfig.initializationVectorLength); // generate key and initialization vector // the constants are defined in the .env file(s)
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv); // create a decipher object
+    let plainbytes = decipher.update(cipherbytes); // decrypt the ciphertext bytes
+    plainbytes = Buffer.concat([plainbytes, decipher.final()]); // apply the encryption and destroy the cipher object
+    let plaintext = new TextDecoder().decode(plainbytes); // decode the decrypted bytes as a string
+    console.timeEnd('decrypt');
+    return String(plaintext); // return the decrypted bytes and invalidates the decipher object
+};
