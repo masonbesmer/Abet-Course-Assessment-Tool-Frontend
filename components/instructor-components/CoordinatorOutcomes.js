af6a9960785633e5478808d8b586b4056d9fae3d
Laymon, Michael <michaellaymon@my.unt.edu>
2023-04-19 21:53:20 -0500

    Begin work on instructor form updates

--- components/instructor-components/CoordinatorOutcomes.js
+++ components/instructor-components/CoordinatorOutcomes.js
@@ -12,13 +12,13 @@ const CoordinatorOutcomes = ({outcome, index, result}) => {
                 <div className="vertical-container">
                     <Text fontSize="lg" fontWeight="bold">Student Work Used</Text>
                     <List styleType="disc">
-                        {/* insert list of student work attachments here */}
+                        {/* TODO: insert list of student work attachments here */}
                     </List>
                 </div>
                 <div className="vertical-container">
                     <Text fontSize="lg" fontWeight="bold">Number of Students Who Achieved 75% of this Outcome</Text>
                     <div className="student-outcome-container">
-                        {/* insert percentages of student results here */}
+                        {/* TODO: insert percentages of student results here */}
                     </div>
                 </div>
             </div>
--- components/instructor-components/OutcomeInput.js
+++ components/instructor-components/OutcomeInput.js
@@ -68,7 +68,7 @@ const OutcomeInput = ({outcome, index, studentsByMajor, currentStudentWorks}) =>
                     <List styleType="disc">
                         {renderStudentWork}
                     </List>
-                    <form onSubmit = {addStudentWork}>
+                    <form onSubmit={addStudentWork}>
                         <Input 
                             placeholder="add student work" 
                             variant="flushed" 
