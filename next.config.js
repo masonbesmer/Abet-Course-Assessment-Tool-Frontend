9db7813467e508331282b6d746f55f6378acd35a
Laymon, Michael <michaellaymon@my.unt.edu>
2023-05-06 13:39:34 -0500

    Add environment variable for server in dev & prod Configure build directory

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}.env.local`}) // Include varaibles from .env.local
+dotenv.config({ path: `${__dirname}.env.production`, override: true }) // Include varaibles from .env.development
 dotenv.config({ path: `${__dirname}.env.development`, override: true }) // Include varaibles from .env.local
 
 const nextConfig = {
     env: {
-        SURVEY_JWT_KEY:   process.env.SURVEY_JWT_SECRET,
+        BACKEND: process.env.BACKEND,
+        SURVEY_JWT_KEY: process.env.SURVEY_JWT_SECRET,
     },
+    distDir: 'build',
 };
 
 module.exports = nextConfig;
